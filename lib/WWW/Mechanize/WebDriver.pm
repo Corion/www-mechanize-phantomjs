7b425b9bd43b502b5df03db57c58231db9da5717
Max Maischein <corion@corion.net>
2013-04-13 00:50:25 +0200

    Add ->content, ->xpath dummy
    Also, documentation for ->selector,

--- lib/WWW/Mechanize/WebDriver.pm
+++ lib/WWW/Mechanize/WebDriver.pm
@@ -1,6 +1,5 @@
 package WWW::Mechanize::WebDriver;
 use strict;
-
 use Selenium::Remote::Driver;
 use WWW::Mechanize::Plugin::Selector;
 
@@ -46,8 +45,36 @@ sub decoded_content {
     $_[0]->driver->get_page_source
 };
 
+sub content {
+    $_[0]->driver->get_page_source
+};
+
 sub title {
     $_[0]->driver->get_title;
 };
+
+=head2 C<< $mech->selector( $css_selector, %options ) >>
+
+  my @text = $mech->selector('p.content');
+
+Returns all nodes matching the given CSS selector. If
+C<$css_selector> is an array reference, it returns
+all nodes matched by any of the CSS selectors in the array.
+
+This takes the same options that C<< ->xpath >> does.
+
+This method is implemented via L<WWW::Mechanize::Plugin::Selector>.
+
+=cut
+
+*selector = \&WWW::Mechanize::Plugin::Selector::selector;
+
+sub xpath {
+    my( $self, $query, %options) = @_;
+
+    # XXX Determine if we want only one element
+    #     or a list, like WWW::Mechanize::Firefox
+    $_[0]->driver->find_elements($query);
+}
 
 1;
