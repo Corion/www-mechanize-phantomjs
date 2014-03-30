11ce544f0f66a066c319ec2a963121b233cd0f78
Max Maischein <corion@corion.net>
2014-03-30 13:46:22 +0200

    Add example generator, also add some documentation to example scripts

--- examples/dump-links.pl
+++ examples/dump-links.pl
@@ -12,3 +12,17 @@ sleep 5;
 print $_->get_attribute('href'), "\n\t-> ", $_->get_attribute('innerHTML'), "\n"
   for $mech->selector('a.download');
 
+=head1 NAME
+
+dump-links.pl - Dump links on a webpage
+
+=head1 SYNOPSIS
+
+dump-links.pl
+
+=head1 DESCRIPTION
+
+This program demonstrates how to read elements out of the PhantomJS
+DOM and how to get at text within nodes.
+
+=cut
--- examples/javascript.pl
+++ examples/javascript.pl
@@ -1,3 +1,4 @@
+#!perl -w
 use strict;
 use WWW::Mechanize::PhantomJS;
 
@@ -11,3 +12,18 @@ $mech->get_local('links.html');
 print $mech->eval_in_page(<<'JS');
     ["Just","another","Perl","Hacker"].join(" ");
 JS
+
+=head1 NAME
+
+javascript.pl - execute Javascript in a page
+
+=head1 SYNOPSIS
+
+javascript.pl
+
+=head1 DESCRIPTION
+
+B<This program> demonstrates how to execute simple
+Javascript in a page.
+
+=cut
