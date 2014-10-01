6c82197d682c6e1a5f69ba9706656e39f29b34e8
Max Maischein <corion@corion.net>
2014-10-01 19:39:09 +0200

    Bump version

--- lib/WWW/Mechanize/PhantomJS.pm
+++ lib/WWW/Mechanize/PhantomJS.pm
@@ -11,7 +11,7 @@ use WWW::Mechanize::Link;
 use IO::Socket::INET;
 
 use vars qw($VERSION %link_spec);
-$VERSION= '0.09';
+$VERSION= '0.10';
 
 =head1 NAME
 
--- lib/WWW/Mechanize/PhantomJS/DSL.pm
+++ lib/WWW/Mechanize/PhantomJS/DSL.pm
@@ -5,7 +5,7 @@ use Object::Import;
 use Carp qw(croak);
 
 use vars qw($VERSION @CARP_NOT);
-$VERSION= '0.09';
+$VERSION= '0.10';
 
 @CARP_NOT = (qw[
     WWW::Mechanize::PhantomJS
