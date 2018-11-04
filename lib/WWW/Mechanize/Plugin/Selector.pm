64dff0dffdfec54cdd26cf7fb7883d90c1a0b325
Max Maischein <corion@corion.net>
2018-11-04 16:18:43 +0100

    Update Changes, bump version

--- lib/WWW/Mechanize/Plugin/Selector.pm
+++ lib/WWW/Mechanize/Plugin/Selector.pm
@@ -1,7 +1,7 @@
 package WWW::Mechanize::Plugin::Selector;
 use strict;
 use vars qw($VERSION);
-$VERSION= '0.21';
+$VERSION= '0.22';
 use HTML::Selector::XPath 'selector_to_xpath';
 
 =head1 SYNOPSIS
